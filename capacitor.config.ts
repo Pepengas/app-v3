819250914e73f145ed920cdaa4451d0178c04362
Pepengas <jotarujojo@gmail.com>
2025-06-16 15:57:47 +0300

    Add Android platform and Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.universitycompanion',
+  appName: 'university-companion',
+  webDir: 'dist'
+};
+
+export default config;
